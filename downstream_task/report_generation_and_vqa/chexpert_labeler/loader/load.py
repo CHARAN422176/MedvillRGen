78fffe81e1b2ad405aafe0ef063f43a3b65cb25e
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-07-04 14:44:55 +0530

    commit

--- downstream_task/report_generation_and_vqa/chexpert_labeler/loader/load.py
+++ downstream_task/report_generation_and_vqa/chexpert_labeler/loader/load.py
@@ -3,7 +3,7 @@ import warnings
 import re
 import bioc
 import pandas as pd
-from negbio.negbio.pipeline import text2bioc, ssplit, section_split
+from chexpert_labeler.negbio.negbio.pipeline import text2bioc, ssplit, section_split
 
 from constants import *
 
